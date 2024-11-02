6b70184fa991de3b86290dcfafbc8e8f08591d53
Jay Potter <j@ypotter.ca>
2024-11-02 19:22:15 -0400

    Update AbstractStringable.php

--- src/Potter/Stringable/AbstractStringable.php
+++ src/Potter/Stringable/AbstractStringable.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Potter\Stringable;
 
+use Potter\{ArrayAccess\ArrayAccessInterface, Json\Serializable\JsonSerializableInterface};
 use Illuminate\Support\Stringable;
 
-abstract class AbstractStringable extends Stringable implements StringableInterface
+abstract class AbstractStringable extends Stringable implements ArrayAccessInterface, 
+                                                                JsonSerializableInterface, 
+                                                                StringableInterface
 { }
