bacdf1e192e412bd9f1af05ea9bd9550dff53ddc
Jay Potter <j@ypotter.ca>
2024-11-02 19:08:38 -0400

    Create AbstractStringable.php

---
+++ src/Potter/Stringable/AbstractStringable.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Stringable;
+
+use Illuminate\Support\Stringable;
+
+abstract class AbstractStringable extends Stringable implements StringableInterface
+{ }
