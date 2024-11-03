3a0beb54dcaf9bed931332f4fe66df879b6a8c7c
Jay Potter <j@ypotter.ca>
2024-11-03 15:53:23 -0500

    Update AbstractStringable.php

--- src/PHPAbles/Stringable/AbstractStringable.php
+++ src/PHPAbles/Stringable/AbstractStringable.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace PHPAbles\Stringable;
 
 use Potter\ArrayAccess\ArrayAccessInterface,
-    PHPAbles\Json\Serializable\JsonSerializableInterface,
+    PHPAbles\Json\JsonSerializableInterface,
     Illuminate\Support\Stringable;
 
 abstract class AbstractStringable extends Stringable implements ArrayAccessInterface, 
